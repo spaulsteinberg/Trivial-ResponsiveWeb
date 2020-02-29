f73aa4d14e5806ac354fd18ecc7d34343d00dbe2
spaulsteinberg <sampaulsteinberg@gmail.com>
2020-02-29 12:48:21 -0500

    removing test commit

--- myCtrl.js
+++
@@ -1,8 +0,0 @@
-//practice with a first controller, scope allows for html use
-app.controller("myCtrl", function($scope) {
-    $scope.firstName = "Uel";
-    $scope.lastName = "Bergsam";
-    var num1 = 5;
-    var num2 = 6;
-    $scope.product = num1 * num2;
-});
