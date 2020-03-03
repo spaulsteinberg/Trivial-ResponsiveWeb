347656bf05b57987d5ceb4c8c40cd67788c5c6c6
sampaulsteinberg <sampaulsteinberg@gmail.com>
2020-03-02 21:00:09 -0500

    added some http

---
+++ src/scripts/controllers/httpRequests.js
@@ -0,0 +1,23 @@
+
+//controller to make http request. 
+app.controller("funcGet", function($scope, $http){
+        $http({
+          method : "GET", //specify method
+          url : "welcome.htm" //specify URL
+        }).then(function onSuccess(response) {//functions after then will be for response, then error.\
+            $scope.response = response.status + " " + response.statusText;
+          }, function onError(response) {
+            $scope.response = response.status + " " + response.statusText;
+        });
+});
+
+//controller to retrieve names from php form
+app.controller("namesFromServer", function($scope, $http){
+    $http.get("https://www.w3schools.com/angular/customers.php").then(function(response){
+        $scope.myData = response.data.records; //records is an object containing a list of objects with a 'Name' and 'Country'
+    });
+
+    $scope.sortBy = function(method){
+        $scope.sortMethod = method;
+    }
+});
